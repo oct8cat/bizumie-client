6e18bddb95fd2d0295c1edfc59dc1343e0995071
Sergey Elsukov <oct8cat@gmail.com>
2018-01-22 11:52:19 +0300

    UploadList

--- src/components/UploadList.js
+++ src/components/UploadList.js
@@ -2,7 +2,7 @@ const React = require('react')
 const {graphql} = require('react-apollo')
 const {Card, Message} = require('semantic-ui-react')
 const UploadPlus = require('./UploadPlus')
-const {isEmpty, map} = require('ramda')
+const {map} = require('ramda')
 const {uploadsQuery} = require('../queries')
 const {SERVER_URL} = require('../const')
 
@@ -11,25 +11,19 @@ module.exports = graphql(uploadsQuery)(({data: {error, loading, ...data}}) => {
   if (error || loading) return null
 
   return (
-    <div>
-      {(isEmpty(data.uploads)) ? (
-        'No uploads'
-      ) : (
-        <Card.Group itemsPerRow={6}>
-          {map((upload) => (
-            <Card
-              key={upload.id}
-              meta={{content: upload.originalName, textAlign: 'center'}}
-              image={`${SERVER_URL}${upload.url}`}
-            />
-          ), data.uploads)}
-          <Card>
-            <Card.Content style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-              <UploadPlus />
-            </Card.Content>
-          </Card>
-        </Card.Group>
-      )}
-    </div>
+    <Card.Group itemsPerRow={6}>
+      {map((upload) => (
+        <Card
+          key={upload.id}
+          meta={{content: upload.originalName, textAlign: 'center'}}
+          image={`${SERVER_URL}${upload.url}`}
+        />
+      ), data.uploads)}
+      <Card>
+        <Card.Content style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <UploadPlus />
+        </Card.Content>
+      </Card>
+    </Card.Group>
   )
 })
